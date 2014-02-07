159328c85a1c634c8a38367e2cba3b0b8905cfc5
Daniel <daniel.maansson@gmail.com>
2014-02-07 18:49:43 +0100

    Fixed MainView

--- src/se/kth/csc/iprog/dinnerplanner/swing/view/MainView.java
+++ src/se/kth/csc/iprog/dinnerplanner/swing/view/MainView.java
@@ -11,28 +11,32 @@ import se.kth.csc.iprog.dinnerplanner.model.DinnerModel;
 public class MainView extends JPanel {
 	
 	private static final long serialVersionUID = 1L;
-	
-	DishListView dishes;
+
 	DishDetails dishDetails;
+	DishListView dishes;
 	DinnerModel wholeModel;
 	JSplitPane split;
 	DinnerListView dinnerListView;
 	
 	public MainView(DinnerModel instancedModel){
 		wholeModel = instancedModel;
-		
 		DishDetails.OpenWindow();
 		
-
+		this.setLayout(new BorderLayout());
+		
 		dishes = new DishListView();
-		dishes.setMinimumSize(new Dimension(500,50));
+		dishes.setMinimumSize(new Dimension(500, 50));
+		
 		dinnerListView = new DinnerListView(); 
 				
 		split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
 				dishes,
 				dinnerListView);
 		
-		//this.add(dishDetails, BorderLayout.CENTER);
+		split.setResizeWeight(1.0);
+		split.setDividerLocation(150);
+		
+		this.add(split, BorderLayout.CENTER);
 	}
 	
 }
