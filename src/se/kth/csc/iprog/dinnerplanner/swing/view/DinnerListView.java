dfa70a27c8d6e358d334592f654af31834e38005
Daniel <daniel.maansson@gmail.com>
2014-02-07 19:32:32 +0100

    Nice title

--- src/se/kth/csc/iprog/dinnerplanner/swing/view/DinnerListView.java
+++ src/se/kth/csc/iprog/dinnerplanner/swing/view/DinnerListView.java
@@ -1,6 +1,8 @@
 package se.kth.csc.iprog.dinnerplanner.swing.view;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridLayout;
 
 import javax.swing.JButton;
@@ -10,6 +12,7 @@ import javax.swing.JScrollPane;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerModel;
 import javax.swing.SpinnerNumberModel;
+import javax.swing.border.EmptyBorder;
 
 public class DinnerListView extends JPanel{
 
@@ -23,15 +26,16 @@ public class DinnerListView extends JPanel{
 
 		//Top
 		JPanel top = new JPanel();
-		top.setLayout(new GridLayout(2,2));
-		top.add(new JLabel("test 1"));
+		top.setLayout(new GridLayout(2,2, 0, 10));
+		top.add(new JLabel("Number of people: "));
+		top.setBorder(new EmptyBorder(10, 30, 10, 30) );
 
 		SpinnerModel model = new SpinnerNumberModel(2, 1, 100, 1);
 		JSpinner numPeople = new JSpinner(model);
 		
 		top.add(numPeople);
-		top.add(new JLabel("test 3"));
-		top.add(new JLabel("test 4"));
+		top.add(new JLabel("Total cost: "));
+		top.add(new JLabel("$ 943.00"));
 		
 		this.add(top, BorderLayout.NORTH);
 		
@@ -40,10 +44,13 @@ public class DinnerListView extends JPanel{
 		middle.setLayout(new BorderLayout());
 		
 		dishes = new DinnerDishList();
-		middle.add(new JLabel("Dinner menu"), BorderLayout.NORTH);
+		JLabel title = new JLabel("Dinner menu", JLabel.CENTER);
+		title.setFont(new Font(title.getFont().getName(), Font.PLAIN, 30));
+		middle.add(title, BorderLayout.NORTH);
 		
 		JScrollPane scroll = new JScrollPane(dishes);
 		middle.add(scroll, BorderLayout.CENTER);
+		scroll.setBorder(new EmptyBorder(0,0,0,0));
 
 		this.add(middle, BorderLayout.CENTER);
 		
